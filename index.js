6531cb42a5a5b926c1de1132733d57e5cdbb8267
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-01-23 13:35:28 -0800

    Added wednesday MVP

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //imports
-require('dotenv').config();
+require('dotenv').config(); //i do not understand how this line works
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
@@ -16,11 +16,28 @@ const db = knex(knexConfig.development);
 server.use(helmet());
 server.use(express.json());
 server.use(morgan());
+function lock(req, res, next) {
+	const token = req.headers.authorization;
+	if (token) {
+		jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+			if (err) {
+				res.status(401).json({message: 'token invalid'});
+			} else {
+				req.decodedToken = decodedToken;
+				next();
+			}
+		});
+	} else {
+		res.status(401).json({message: 'no token provided'});
+	}
+}
+
+//helpers
 function generateToken(user) {
 	const payload = {
 		username: user.username
 	};
-	const secret = process.env.JWT_SECRET;
+	const secret = process.env.JWT_SECRET; //i do not understand how this line works
 	const options = {expiresIn: '10m'};
 
 	return jwt.sign(payload, secret, options);
@@ -60,6 +77,15 @@ server.post('/api/login', (req, res) => {
 			} else {
 				res.status(401).json({you: 'Incorrect username or password'});
 			}
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+server.get('/api/users', lock, (req, res) => {
+	db('users')
+		.select('id', 'username')
+		.then(users => {
+			res.status(200).json({users, decodedToken: req.decodedToken});
 		})
 		.catch(err => res.status(500).json(err));
 });
